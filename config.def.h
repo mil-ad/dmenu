9ee50f7ad1fa07b922fd015e053eb93c52632efa
Milad Alizadeh <hi@mil.ad>
2020-11-08 11:49:17 +0000

    Switched to Gruvbox colours

--- config.def.h
+++ config.def.h
@@ -16,7 +16,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#1d2021", "#d79921" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
